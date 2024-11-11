070f150fe1268c043cf2b595a1c97c528c5c8f41
velobosv1977 <velobosv33@gmail.com>
2024-11-11 12:21:05 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,88 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("ДЗ 3. Переменные");
+        System.out.println("Задача 1. Возраст совершеннолетия");
+        // Задание возраста
+        int age = 14;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то возраст совершеннолетия не наступил");
         }
+        System.out.println("Задача 2. Температура и шапка");
+        //Задание температуры
+        int t=8;
+        //Условие
+        if (t>=5) {
+            System.out.println("Сегодня тепло, "+t+"гр.С. Можно идти без шапки");
+        } else {
+            System.out.println("Сегодня холодно, " + t + "гр.С. Наденьте шапку");
+        }
+        System.out.println("Задача 3. Превышение скорости");
+        //Задание скорости
+        int speed=50;
+        //Условие
+        if (speed<=60) {
+            System.out.println("Если скорость меньше 60 км/ч, то можно ездить спокойно. Ваша скорость "+speed+ " км/ч.");
+        } else {
+            System.out.println("Ваша скорость "+speed+" км/ч. Это больше допустимого. Придется заплатить штраф");
+        }
+        System.out.println("Задача 4. Распределение по возрасту");
+        //Задание возраста человека
+        int age1=24;
+        //Условия выборки
+        if (age1>=2 && age1<=6) {
+            System.out.println("Если возраст человека равен "+age1+" , то ему нужно ходить в детский сад.");
+        }
+        if (age1>=7 && age1<=17) {
+            System.out.println("Если возраст человека равен "+age1+" , то ему нужно ходить школу.");
+        }
+        if (age1>=18 && age1<=24) {
+            System.out.println("Если возраст человека равен "+age1+" , то его место в унивреситете.");
+        }
+        if (age1>24) {
+            System.out.println("Если возраст человека равен "+age1+" , то ему пора идти на работу.");
+        }
+        System.out.println("Задача 5. Ограничение по возрасту на аттракционе");
+        //Задание возраста ребенка
+        int age2=10;
+        //Условия выборки
+        if (age2<5) {
+            System.out.println("Если возраст ребёнка равен "+age2+", то ему нельзя кататься на аттракционе.");
+        }
+        else if (age2>=5 && age2<14) {
+            System.out.println("Если возраст ребёнка равен "+age2+", то можно кататься на аттракционе в сопровождении взрослого.");
+        }
+        else if (age2>=14) {
+            System.out.println("Если возраст ребёнка равен " + age2 + ", то можно кататься на аттракционе без сопровождения взрослого.");
+        }
+        System.out.println("Задача 6. Вместимость вагона.");
+        //Задание кол-ва пассажиров в вагоне
+        int passenger=100;
+        //Условия выборки
+        if (passenger>102) {
+            System.out.println("В вагоне месть нет");
+        }
+        else if (passenger>1 && passenger<=60) {
+            System.out.println("В вагоне сейчас "+passenger+" чел. Есть сидячие места");
+        }
+        else if (passenger>60 && passenger<=102) {
+            System.out.println("В вагоне сейчас "+passenger+" чел. Есть стоячие места");
+        }
+        System.out.println("Задача 7. Сравнение чисел.");
+        //Задание чисел
+        int one=7;
+        int two=12;
+        int three=8;
+        //Условия сравнения чисел
+        if (one>two && one>three) {
+            System.out.println("Число one="+one+" больше остальных");
+        } else if (two>one && two>three) {
+            System.out.println("Число two="+two+" больше остальных");
+        } else if (three>one && three>two) {
+            System.out.println("Число three="+three+" больше остальных");
+        }
+
+
     }
 }
